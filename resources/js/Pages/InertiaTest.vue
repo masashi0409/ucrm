716157d7de725bd7fb04f5e46e2654b4271cca48
shimano masashi <masashi.shimano0409@gmail.com>
2023-09-15 15:55:52 +0900

    index,show

--- resources/js/Pages/InertiaTest.vue
+++ resources/js/Pages/InertiaTest.vue
@@ -7,5 +7,7 @@ import { Link } from '@inertiajs/inertia-vue3';
 test<br>
 
 <a href="/">aタグ</a><br>
-<Link href="/">Link</Link>
+<Link href="/">Link</Link><br>
+<Link :href="route('inertia.index')">index</Link><br>
+<Link :href="route('inertia.show', { id: 1 })">show</Link><br>
 </template>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\InertiaTestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +20,9 @@ use Inertia\Inertia;
 Route::get('/inertia-test', function () {
     return Inertia::render('InertiaTest');
 });
+
+Route::get('/inertia/index', [InertiaTestController::class, 'index'])->name('inertia.index');
+Route::get('inertia/show/{id}', [InertiaTestController::class, 'show'])->name('inertia.show');
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
