66bae8cd65c1550f0037d00965fb74615701c998
shimano <masashi.shimano0409@gmail.com>
2024-02-12 22:34:53 +0900

    購買履歴一覧表示

--- resources/js/Pages/Purchases/Index.vue
+++ resources/js/Pages/Purchases/Index.vue
@@ -10,11 +10,85 @@ const props = defineProps({
     totals: Object
 })
 
-onMounted( () =>{
-    console.log(props.totals.data)
+const totalsPaging = reactive({})
+const totals = ref([])
+
+onMounted(() => {
+    // console.log(props.totals) 
+    // console.log(props.totals.data)
+    totalsPaging.value = props.totals // paginateで取得しているのでページングの情報を含む
+    totals.value = props.totals.data // paginateの場合dataに取得データの配列が入っている
 })
 
 </script>
 <template>
+    <Head title="購買履歴" />
 
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                購買履歴
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        <section class="text-gray-600 body-font">
+                            <div class="container px-5 py-8 mx-auto">
+                                <FlashMessage />
+                                <div class="flex pl-4 my-4 lg:w-2/3 w-full mx-auto">
+                                </div>
+                                <div class="lg:w-2/3 w-full mx-auto overflow-auto">
+                                    <table class="table-auto w-full text-left whitespace-no-wrap">
+                                        <thead>
+                                            <tr>
+                                                <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                                                    購買履歴ID
+                                                </th>
+                                                <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">
+                                                    会員氏名
+                                                </th>
+                                                <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                                                    合計金額
+                                                </th>
+                                                <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                                                    ステータス
+                                                </th>
+                                                <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                                                    購入日
+                                                </th>
+                                            </tr>
+                                        </thead>
+                                        <tbody v-if="totals">
+                                            <tr v-for="total in totals" :key="total.id">
+                                                <td class="border-b-2 border-gray-200 px-4 py-3">
+                                                        {{ total.id }}
+                                                </td>
+                                                <td class="border-b-2 border-gray-200 px-4 py-3">
+                                                    {{  total.customer_name }}
+                                                </td>
+                                                <td class="border-b-2 border-gray-200 px-4 py-3">
+                                                    {{ total.total }}
+                                                </td>
+                                                <td class="border-b-2 border-gray-200 px-4 py-3">
+                                                    {{ total.status }}
+                                                </td>
+                                                <td class="border-b-2 border-gray-200 px-4 py-3">
+                                                    {{ total.created_at }}
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                            <Pagination v-if="totalsPaging.value"
+                                class="mt-6" :links="totalsPaging.value.links"></Pagination>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
 </template>
